2ee215e9a2f1f15f06b48924b35a3dd48e8a2916
Layan Yashoda <layanyashoda@gmail.com>
2024-07-14 01:21:45 +0530

    Next Button

--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:trailbuddy/utils/constants/image_strings.dart';
 import 'package:trailbuddy/utils/constants/text_strings.dart';
 import 'package:trailbuddy/utils/constants/sizes.dart';
 import 'package:trailbuddy/utils/helpers/helper_functions.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
+
+  @override
+  _OnboardingScreenState createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController();
+  bool _showSkipButton = true;
+
+  void _skipToLastPage() {
+    _pageController.jumpToPage(2); // Index of the last onboarding page
+  }
+
+  void _nextPage() {
+    if (_pageController.page != null && _pageController.page!.round() < 2) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      // Navigate to the next screen after onboarding
+      // Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => NextScreen()));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        _showSkipButton = _pageController.page?.round() == 0 || _pageController.page?.round() == 1;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +55,8 @@ class OnboardingScreen extends StatelessWidget {
         children: [
           /// Horizontal Scrollable Page
           PageView(
+            controller: _pageController,
+            physics: const BouncingScrollPhysics(),
             children: const [
               OnBoardingPage(
                 image: TrailImages.onBoardingImage1,
@@ -32,13 +75,60 @@ class OnboardingScreen extends StatelessWidget {
               ),
             ],
           ),
-
-
-
-
-
-
-
+          // Skip button
+          Positioned(
+            top: 40.0,
+            right: 20.0,
+            child: _showSkipButton
+                ? TextButton(
+              onPressed: _skipToLastPage,
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              ),
+              child: const Text(
+                'Skip',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.blue,
+                ),
+              ),
+            )
+                : Container(),
+          ),
+          // Smooth page indicator
+          Positioned(
+            bottom: 50.0,
+            left: 20.0,
+            child: SmoothPageIndicator(
+              controller: _pageController,  // PageController
+              count: 3,
+              effect: const ExpandingDotsEffect(
+                activeDotColor: Colors.blue,
+                dotHeight: 8.0,
+                dotWidth: 8.0,
+                expansionFactor: 4.0,
+                spacing: 4.0,
+                dotColor: Colors.grey,
+              ),
+            ),
+          ),
+          // Next button
+          Positioned(
+            bottom: 20.0,
+            right: 20.0,
+            child: FloatingActionButton(
+              onPressed: _nextPage,
+              backgroundColor: Colors.blue,
+              child: const Icon(
+                Icons.arrow_forward,
+                color: Colors.white,
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -54,20 +144,40 @@ class OnBoardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(TrailSizes.defaultSpace),
-      child: Column(
-        children: [
-          Image(
-            width: TrailHelperFunctions.screenWidth() * 0.8,
-            height: TrailHelperFunctions.screenHeight() * 0.6,
-            image: AssetImage(image),
+    return Stack(
+      children: [
+        // Background Image
+        Positioned.fill(
+          child: Image.asset(
+            image,
+            fit: BoxFit.cover,
           ),
-          Text(title, style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center),
-          const SizedBox(height: TrailSizes.spaceBtwItems),
-          Text(subTitle, style: Theme.of(context).textTheme.bodyMedium, textAlign: TextAlign.center),
-        ],
-      ),
+        ),
+        // Title and Subtitle
+        Positioned(
+          bottom: 100.0,
+          left: 0.0,
+          right: 0.0,
+          child: Padding(
+            padding: const EdgeInsets.all(TrailSizes.defaultSpace),
+            child: Column(
+              children: [
+                Text(
+                  title,
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: TrailSizes.spaceBtwItems),
+                Text(
+                  subTitle,
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
